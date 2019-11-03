c30835140baca9aaa0f58f70fc7e6637959bc1a0
Maher Wardak <maher.wardak@gmail.com>
2019-11-02 23:25:00 -0400

    added src and new Date() in JS

---
+++ script.js
@@ -0,0 +1,17 @@
+// let i = 0;
+
+// function increment() {
+//   i++;
+//   console.log(i);
+// }
+
+// setInterval(increment, 1000)
+
+
+
+let today = new Date();
+let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+console.log(time);
+
+// const time = document.getElementById("time");
+
