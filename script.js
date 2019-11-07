a78f260dab0b4673002ade0207e1dd4db93fee2a
Maher Wardak <maher.wardak@gmail.com>
2019-11-07 12:56:56 -0500

    date has been added

--- script.js
+++ script.js
@@ -1,17 +1,47 @@
 
-
-let i = 0;
+// display a basic clock 
+// let i = 0;
 
 function increment() {
     let today = new Date();
-    // let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-    // const clock = time;
+    let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    const clock = time;
+    // const ampm = today.getHours() < 12? "AM" : "PM";
 
     const paragraph = document.getElementById("tmn");
-    paragraph.textContent = today;
-   
+    paragraph.textContent = clock;
+
     i++;
     console.log(i);
+
+ 
 }
 
+//display the month and weekday
+
+    const dte = new Date();
+
+    const monthly= ['January','February','March','April','May','June','July','August','September','October','November','December'];
+    const weekday = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+    const year = 2019;
+
+    
+    const month = monthly[dte.getMonth()];
+    const mDay = weekday[dte.getDay()];
+    const nDay = dte.getDate();
+
+    const setDate =  mDay + ", " + month + ", "+ nDay + ", " + year;
+
+    const x = document.getElementById("mw"); 
+    x.textContent = setDate; 
+
+    console.log(setDate);
+
+
+
+
+
+
 setInterval(increment, 1000);
+
+
