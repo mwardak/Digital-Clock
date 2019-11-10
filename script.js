85c5629c006d25f0837cd3f4aa1d185b54403b25
Maher Wardak <maher.wardak@gmail.com>
2019-11-09 21:27:22 -0500

    AM:PM to display

--- script.js
+++ script.js
@@ -1,21 +1,32 @@
 
-// display a basic clock 
-// let i = 0;
+// display clock with AM:PM
+let i = 0;
 
 function increment() {
     let today = new Date();
     let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
     const clock = time;
-    // const ampm = today.getHours() < 12? "AM" : "PM";
 
-    const paragraph = document.getElementById("tmn");
-    paragraph.textContent = clock;
+    const paragraph = document.getElementById("tmn").textContent = clock;
+    
+    
+    const militaryTime = ["AM", "PM"]
+
+    if (clock =< 12) {
+        return militaryTime[0];
+    } else {
+        return militaryTime[1];
+}
+   
+    const ampm = document.getElementById("ap").textContent = militaryTime;
+    
 
     i++;
+    
     console.log(i);
-
- 
 }
+
+
 
 //display the month and weekday
 
